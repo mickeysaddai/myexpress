32bd92b91de5a8bf46b8bc27cff31ac968d4ce6d
Michelle Addai <88467155+mickeysaddai@users.noreply.github.com>
2022-01-14 16:33:11 -0500

    ejs installed and tested

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 const express = require('express');   //require express library we donwloaded
 const app = express(); //set up actual server  by calling express() we create an applicaton
 
+
+app.set('view engine', 'ejs')
+
 app.get('/', (req, res) => {
     console.log("Here")
-    res.status(500).json({message: "Error"})
+    res.render("index", {text: "!!!"} )
+    // res.download('server.js')
+    // res.status(500).json({message: "Error"})
 
 })  //app takes 3 parameters:requst, response and next although next is usually not used
 
